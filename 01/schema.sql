0c3bac2ff2630b640da762dce5c097a8be286574
Robert Stachowski <Robertstachowski0@gmail.com>
2026-01-21 12:33:55 +0100

    Create posts table and insert sample records with timestamps

---
+++ 01/schema.sql
@@ -0,0 +1,19 @@
+
+
+
+CREATE TABLE IF NOT EXISTS posts(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    content TEXT,
+    created_at TIMESTAMP DEFAULT now()
+);
+
+INSERT INTO posts (title, content)
+VALUES
+('aktualny post', 'aktualny timestamp');
+
+INSERT INTO posts (title, content, created_at)
+VALUES
+('post wczoraj', 'czas wczoraj', now() - INTERVAL '1 day'),
+('post 3 dni temu', 'czas 3 dni temu', now() - INTERVAL '3 days'),
+('post 10 dni temu', 'czas 10 dni temu', now() - INTERVAL '10 days');
